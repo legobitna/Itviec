bbe9a4a7022ec2c6cf1de5e540ba20ceddd03f47
bitna kim <tallshe108@hanmail.net>
2020-04-17 16:25:01 +0700

    add search function

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from "react";
 import JobList from "../components/JobCard";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Form } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+
 export default function Jobs() {
   let originalList = [];
-  let filteredList = [];
   let [jobs, setJobs] = useState([]);
-  let keyword = "";
+  let [keyword, setKeyword] = useState("");
+  const [encode, setEncode] = useState("");
+  let history = useHistory();
+  let searchTerm = [];
 
   const getData = async () => {
     //let url ="http://localhost:3001/jobs";
@@ -15,14 +19,35 @@ export default function Jobs() {
     let data = await fetch(url);
     let result = await data.json();
     originalList = result;
-
-    setJobs(result);
+    getQuery(originalList);
   };
+
+  const getQuery = (arr) => {
+    let filteredList = [];
+    let queryString = window.location.search;
+    let splitQueryString = queryString.split("?q=");
+    if (splitQueryString.length > 1) {
+      searchTerm = splitQueryString[1].split("%20");
+      filteredList = arr.filter((item) =>
+        searchTerm.some((key) =>
+          item.title.toLowerCase().includes(key.toLowerCase())
+        )
+      );
+    } else {
+      filteredList = arr;
+    }
+    setJobs(filteredList);
+  };
+
   useEffect(() => {
     getData();
-  }, []);
+  }, [encode]);
 
-  const search = () => {};
+  const search = (e) => {
+    e.preventDefault();
+    setEncode(encodeURIComponent(keyword));
+    history.push("/jobs/?q=" + encodeURIComponent(keyword));
+  };
 
   return (
     <div className="App">
@@ -36,32 +61,35 @@ export default function Jobs() {
               src="https://itviec.com/assets/logo-itviec-65afac80e92140efa459545bc1c042ff4275f8f197535f147ed7614c2000ab0f.png"
             />
           </Col>
-          <Row className="search-form-wrapper">
-            <Col xs={12} md={10}>
-              <div className="search-section-wrapper">
-                <Row className="search-field-wrapper" noGutters={true}>
-                  <FontAwesomeIcon
-                    icon={faSearch}
-                    style={{ marginRight: "10px", marginLeft: "10px" }}
-                  />
-
-                  <Col col={12}>
-                    <input
-                      type="text"
-                      className="search-box"
-                      placeholder="Keyword skill(Java,IOS...),Job Title,Company..."
-                      onChange={(e) => (keyword = e.target.value)}
+          <Form onSubmit={(e) => search(e)}>
+            <Row className="search-form-wrapper">
+              <Col xs={12} md={10}>
+                <div className="search-section-wrapper">
+                  <Row className="search-field-wrapper" noGutters={true}>
+                    <FontAwesomeIcon
+                      icon={faSearch}
+                      style={{ marginRight: "10px", marginLeft: "10px" }}
                     />
-                  </Col>
-                </Row>
-              </div>
-            </Col>
-            <Col xs={12} md={2}>
-              <button className="search-button" onClick={() => search()}>
-                Search
-              </button>
-            </Col>
-          </Row>
+
+                    <Col col={12}>
+                      <input
+                        value={keyword}
+                        type="text"
+                        className="search-box"
+                        placeholder="Keyword skill(Java,IOS...),Job Title,Company..."
+                        onChange={(e) => setKeyword(e.target.value)}
+                      />
+                    </Col>
+                  </Row>
+                </div>
+              </Col>
+              <Col xs={12} md={2}>
+                <button className="search-button" type="submit">
+                  Search
+                </button>
+              </Col>
+            </Row>
+          </Form>
         </Container>
       </div>
       <Container>
